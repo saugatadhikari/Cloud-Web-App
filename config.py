197af84e3266741e276f4e0591819bb22c71ef47
Saugat <saugat@pc.com>
2022-07-30 14:43:35 -0500

    Add email send and store filename to sql database using rds

---
+++ config.py
@@ -0,0 +1,4 @@
+host = 'cloudprojects.cdyzfzfcsxe4.us-east-2.rds.amazonaws.com'
+user = 'admin'
+pwd = 'password'
+db_name = 'cloudprojects'
--- main.py
+++ main.py
@@ -4,15 +4,28 @@ from datetime import datetime
 
 from flask import Flask, render_template, request
 import boto3
+import pandas as pd
 
+#SQL
+import mysql.connector 
+from mysql.connector import errorcode
+from sqlalchemy import create_engine
+
+import config
 
 app = Flask(__name__)
 
 S3_BUCKET = "saugat"
 from_email = "cloudcomputing@webapp.com"
-# config_set_name = os.environ["SES_CONFIG_SET_NAME"]
-config_set_name = "config_set.yaml"
 
+# SQL connection
+CONNECTION = mysql.connector.connect(
+        host = config.host,
+        user = config.user,
+        password = config.pwd)
+
+CURSOR = CONNECTION.cursor()
+TABLE_NAME = 'filenames_table'
 
 @app.route('/')
 def hello():
@@ -45,11 +58,29 @@ def file_uploader():
 
         # upload file to s3 bucket and get URI
         s3_URI = s3_upload(tmp_filepath, str(f.filename))
+        print('File uploaded successfully')
 
-        send_email(all_emails, s3_URI)
+        try:
+            send_email_test(all_emails, s3_URI)
+        except:
+            return 'Unverified email addresses: please check!'
 
-        #   f.save(secure_filename(f.filename))
-        return 'All files uploaded successfully'
+        # Store filenames to Database
+        try:
+            CURSOR.execute("USE {}".format(config.db_name))
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_BAD_DB_ERROR:
+                db_create(CURSOR, config.db_name)
+                CONNECTION.database = config.db_name
+            else:
+                exit(1)
+        
+        conn_engine = create_engine(f"mysql+mysqlconnector://{config.user}:{config.pwd}@{config.host}/{config.db_name}")
+
+        df = pd.DataFrame([{'filename': f.filename}])
+        df.to_sql(TABLE_NAME, con=conn_engine, if_exists='append')
+
+        return 'Emails sent successfully'
 		
 def s3_upload(filepath, filename):
     # connect to s3
@@ -64,45 +95,50 @@ def s3_upload(filepath, filename):
     return s3_URI
 
 
+def lambda_handler(event, context):
+    send_email_test()
+    
+def send_email_test(all_emails, s3_uri):
+    
+    ses = boto3.client('ses')
 
+    body = f"""
+	    Link of file uploaded to S3 bucket: {s3_uri}
+    """
 
-
-def send_email(emails, s3_URI):
-    client = boto3.client('ses')
-
-    body_html = f"""<html>
-        <head></head>
-        <body>
-          <h2>Link to uploaded file: {s3_URI}</h2>
-          <br/>
-        </body>
-        </html>
-        """
-
-    email_message = {
-        'Body': {
-            'Html': {
-                'Charset': 'utf-8',
-                'Data': body_html,
-            },
-        },
-        'Subject': {
-            'Charset': 'utf-8',
-            'Data': "Cloud Computing Course Project",
-        },
+    ses.send_email(
+	    Source = 'pifibab259@galotv.com',
+	    Destination = {
+		    'ToAddresses': all_emails
+	    },
+	    Message = {
+		    'Subject': {
+			    'Data': 'S3 File URI',
+			    'Charset': 'UTF-8'
+		    },
+		    'Body': {
+			    'Text':{
+				    'Data': body,
+				    'Charset': 'UTF-8'
+			    }
+		    }
+	    }
+    )
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Email successfully sent!')
     }
 
-    ses_response = client.send_email(
-        Destination={
-            'ToAddresses': emails,
-        },
-        Message=email_message,
-        Source=from_email,
-        ConfigurationSetName=config_set_name,
-    )
-
-    print(f"ses response id received: {response['MessageId']}.")
-
+def db_create(cursor, db_name):
+    try:
+        cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(db_name))
+    except mysql.connector.Error as err:
+        print(f"Database could not be created. Error: {err}")
+        exit(1)
+    
+    return
 
 if __name__ == '__main__':
    app.run(port=7000, debug = True)
